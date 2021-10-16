18da49374a1fc9b859f9ebefb54bad74baf8f83d
ajwilson19 <awilson22@zagmail.gonzaga.edu>
2021-10-16 16:02:56 -0700

    galaxy_score, volatility, close

--- module.py
+++ module.py
@@ -1,10 +1,11 @@
 import requests
 import json
 
-link = "https://api.lunarcrush.com/v2?data=assets&key=dj4tgdk9258k4d9yl0ebm&symbol=XRP&data_points=30&interval=hour"
+link = "https://api.lunarcrush.com/v2?data=assets&key=dj4tgdk9258k4d9yl0ebm&symbol=XRP,QNT&data_points=4&time_series_indicators=galaxy_score,volatility,close&indicators=galaxy_score,volatility,close"
 response = requests.get(url=link)
 json_object = json.loads(response.text)
-print(json_object)
+# print(json_object)
 
-# for data in json_object['data']:
-#     print(data['volatility'])
+for data in json_object['data']:
+    print(data['timeSeries'])
+    print()
