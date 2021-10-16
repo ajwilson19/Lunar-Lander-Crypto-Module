b4b1b140676f9e85faab7c0d9a45f79576d148ef
ajwilson19 <awilson22@zagmail.gonzaga.edu>
2021-10-16 16:11:54 -0700

    user lookup

--- module.py
+++ module.py
@@ -1,7 +1,8 @@
 import requests
 import json
 
-link = "https://api.lunarcrush.com/v2?data=assets&key=dj4tgdk9258k4d9yl0ebm&symbol=XRP,QNT&data_points=4&time_series_indicators=galaxy_score,volatility,close&indicators=galaxy_score,volatility,close"
+symbol = input("Enter asset ticker: ")
+link = "https://api.lunarcrush.com/v2?data=assets&key=dj4tgdk9258k4d9yl0ebm&symbol=" + symbol + "&data_points=4&time_series_indicators=galaxy_score,volatility,close&indicators=galaxy_score,volatility,close"
 response = requests.get(url=link)
 json_object = json.loads(response.text)
 # print(json_object)
