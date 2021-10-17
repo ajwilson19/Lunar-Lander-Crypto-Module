c3056228a78930ad21c6ba420bae706204f8b299
ajwilson19 <awilson22@zagmail.gonzaga.edu>
2021-10-16 19:42:12 -0700

    pandas

--- module.py
+++ module.py
@@ -1,13 +1,14 @@
 import requests
 import json
+import pandas as pd
 
 symbol = input("Enter asset ticker: ")
-link = "https://api.lunarcrush.com/v2?data=assets&key=dj4tgdk9258k4d9yl0ebm&symbol=" + symbol + "&data_points=24&time_series_indicators=galaxy_score,volatility,close&indicators=galaxy_score,volatility,close"
+indicators = "galaxy_score,volatility,close"
+
+link = "https://api.lunarcrush.com/v2?data=assets&key=dj4tgdk9258k4d9yl0ebm&symbol=" + symbol + "&data_points=12&time_series_indicators=" + indicators + "&indicators=" + indicators
 response = requests.get(url=link)
 json_object = json.loads(response.text)
 
-
 for data in json_object['data']:
     for hour in data['timeSeries']:
-        print(hour)
-        print()     
+        print(hour['galaxy_score'], hour['volatility'], hour['close'])
