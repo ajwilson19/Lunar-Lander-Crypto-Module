8c8a9d43fdbddab90b47b649eb85c7365bfefe2d
ajwilson19 <awilson22@zagmail.gonzaga.edu>
2021-10-21 15:15:07 -0700

    attributes

--- module.py
+++ module.py
@@ -2,8 +2,9 @@ import requests
 import json
 import pandas as pd
 
-symbol = input("Enter asset ticker: ")
+symbol = "XRP" #input("Enter asset ticker: ")
 data_points = "1" #input("Enter # of hours: ")
+attributes = ["galaxy_score", "volatility", "close", "tweets"]
 
 link = "https://api.lunarcrush.com/v2?data=assets&key=dj4tgdk9258k4d9yl0ebm&symbol=" + symbol + "&data_points=" + data_points + "&interval=hour"
 response = requests.get(url=link)
@@ -12,5 +13,5 @@ json_object = json.loads(response.text)
 data = json_object['data']
 data_dict = data[0]
 for elem in data_dict:
-    if elem in ["galaxy_score", "volatility", "close", "tweets"]:
+    if elem in attributes:
         print(elem, data_dict[elem])
