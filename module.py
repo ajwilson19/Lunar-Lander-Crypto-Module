ebf821ed86140cd8bb82f4f134b3341baca4be99
ajwilson19 <awilson22@zagmail.gonzaga.edu>
2021-10-21 16:06:27 -0700

    --hard code, format fix

--- module.py
+++ module.py
@@ -2,11 +2,11 @@ import requests
 import json
 import pandas as pd
 
-symbol = "XRP" #input("Enter asset ticker: ")
+symbol = input("Enter asset ticker: ")
 data_points = input("Enter # of hours: ")
 attributes = ["galaxy_score", "volatility", "close", "tweets"]
 interval = "hour"
-
+print("Pulling Data...")
 link = "https://api.lunarcrush.com/v2?data=assets&key=dj4tgdk9258k4d9yl0ebm&symbol=" + symbol + "&data_points=" + data_points + "&interval=" + interval
 response = requests.get(url=link)
 json_object = json.loads(response.text)
@@ -14,8 +14,7 @@ json_object = json.loads(response.text)
 data = json_object['data']
 data_dict = data[0]
 for hour in data_dict['timeSeries']:
-    #print(hour)
+    print()
     for elem in hour:
         if elem in attributes:
-            print(elem, hour[elem])
-    print()
+            print(elem, hour[elem])  
