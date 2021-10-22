a6225db4390eaa4141efc705bf987fb26ba92b79
ajwilson19 <awilson22@zagmail.gonzaga.edu>
2021-10-21 20:13:53 -0700

    smol

--- module.py
+++ module.py
@@ -1,14 +1,13 @@
 import requests
 import json
-import pandas as pd
 import matplotlib.pyplot as plt
 
 symbol = input("Enter asset ticker: ")
-data_points = input("Enter # of hours: ")
+data_points = "240" #input("Enter # of hours: ")
 attributes = ["volatility"]
-interval = "hour" #input("hour/day: ")
+
 print("Pulling Data...")
-link = "https://api.lunarcrush.com/v2?data=assets&key=dj4tgdk9258k4d9yl0ebm&symbol=" + symbol + "&data_points=" + data_points + "&interval=" + interval
+link = "https://api.lunarcrush.com/v2?data=assets&key=dj4tgdk9258k4d9yl0ebm&symbol=" + symbol + "&data_points=" + data_points + "&interval=hours"
 response = requests.get(url=link)
 json_object = json.loads(response.text)
 
@@ -21,12 +20,13 @@ data_dict = data[0]
 for hour in data_dict['timeSeries']:
     print()
     for elem in hour:
+        # print(elem, hour[elem])
         if elem in attributes:
             x.append(i)
             i += 1
             y.append(hour[elem])  
 
 plt.plot(x, y)
-plt.xlabel(interval + "s")
+plt.xlabel("hours")
 plt.ylabel(attributes[0])
 plt.show()          
